2d221e09679bd90032f9052dc769b548149499fc
lusine.margarian <lusine.margarian@LAB377.tumo.lab>
2014-07-16 21:38:14 +0400

    added AsyncTask

--- src/com/marakana/android/yamba/StatusActivity.java
+++ src/com/marakana/android/yamba/StatusActivity.java
@@ -2,17 +2,25 @@ package com.marakana.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 public class StatusActivity extends Activity implements OnClickListener {
 
 	private static final String TAG = "StatusActivity";
 	private EditText editStatus;
 	private Button buttonTweet;
+	private TextView textCount;
+	private int defaultTextColor;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,32 @@ public class StatusActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		String status = editStatus.getText().toString();
-		System.out.print("aaa");
 		Log.d(TAG, "OnClick with status:" + status);
 		
-		//new PostTask().execute(status);
+		new PostTask().execute(status);
+	}
+	
+	private final class PostTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			YambaClient yambaCloud = new YambaClient("student", "password");
+			
+			try {
+				yambaCloud.postStatus(params[0]);
+				return "Successfully posted";
+			} catch (YambaClientException e) {
+				e.printStackTrace();
+				return "Failed to post to yamba service";
+			}
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
 		
 	}
 }
