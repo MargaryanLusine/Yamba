c382d96752bb77a37d0ebb5122e64bae232ecc2d
Lusine <lusinemargaryan91@yahoo.com>
2014-08-30 13:04:50 +0400

    ContentProvider

--- src/com/marakana/android/yamba/RefreshService.java
+++ src/com/marakana/android/yamba/RefreshService.java
@@ -11,6 +11,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -32,20 +33,19 @@ public class RefreshService extends IntentService {
 		String username = prefs.getString("username", "");
 		String password = prefs.getString("password", "");
 		
+		// check that username and password are not empty
 		if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
 			Toast.makeText(this, "Please update your status and password", Toast.LENGTH_LONG).show();
 		}
 	
 		Log.d(TAG, "onStarted");
-		
-		DBHelper dbHelper = new DBHelper(this);
-		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		
+	
 		ContentValues values = new ContentValues();
 		
 		YambaClient yambaCloud = new YambaClient(username, password);
 		
 		try {
+			int count = 0;
 			List<Status> timeline = yambaCloud.getTimeline(20);
 			for (Status status:timeline) {
 				values.clear();
@@ -54,7 +54,12 @@ public class RefreshService extends IntentService {
 				values.put(StatusContract.Column.MESSAGE, status.getMessage());
 				values.put(StatusContract.Column.CREATED_AT, status.getCreatedAt().getTime());
 				
-				db.insertWithOnConflict(StatusContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+				Uri uri = getContentResolver().insert(StatusContract.CONTENT_URI, values);
+				
+				if(uri != null) {
+					count++;
+					Log.d("%:s %s" + status.getUser(), status.getMessage());
+				}
 			}
 			
 		} catch (YambaClientException e) {
--- src/com/marakana/android/yamba/StatusContract.java
+++ src/com/marakana/android/yamba/StatusContract.java
@@ -1,14 +1,24 @@
 package com.marakana.android.yamba;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class StatusContract {
 	
+	// DB specific constants
 	public static final String DN_NAME =  "timeline.db";
 	public static final int DB_VERSION = 1;
 	public static final String TABLE = "status";
 	
-	public static final String DEFAULT_SORT = Column.CREATED_AT + " DESC ";
+	// content provider specific constants
+	public static final String AUTHORITY = "com.marakana.android.yamba.StatusProvider";
+	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE);
+	public static final int STATUS_ITEM = 1;
+	public static final int STATUS_DIR = 2;
+	public static final String STATUS_TYPE_ITEM = "vnd.android.cursor.item/vnd.com.marakana.android.yamba.provider.status";
+	public static final String STATUS_TYPE_DIR = "vnd.android.cursor.dir/vnd.com.marakana.android.yamba.provider.status";
+			
+	static final String DEFAULT_SORT = Column.CREATED_AT + " DESC ";
 	
 	public class Column {
 		public static final String ID = BaseColumns._ID;
